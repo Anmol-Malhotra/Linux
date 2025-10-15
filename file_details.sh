d8f1f7d38657ffd403dabe94030b4a308cc208f5
Anmol-Malhotra <anmolmalhotra267@gmail.com>
2025-10-15 09:22:13 +0000

    Shell Scripts

--- file_details.sh
+++ file_details.sh
@@ -4,13 +4,13 @@ echo "Enter a filename:"
 read file
 
 if [ ! -f "$file" ]; then                       ## -f checks if the file exists and is a regular file
-        echo "Error: file '$1' not found."
+        echo "Error: file '$file' not found."
         exit 1
 fi
 
 lines=$(wc -l < "$file")                       ## wc -l counts lines.
 words=$(wc -w < "$file")                       ## wc -w counts words.
-chars=$(wc -m < "$file")                       ## wc -w counts characters.
+chars=$(wc -m < "$file")                       ## wc -m counts characters.
 
 echo "Lines: $lines"
 echo "Words: $words"
