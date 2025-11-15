241d3a7c0ba4c459757907dc9c075b370ea18cbc
Anmol-Malhotra <anmolmalhotra267@gmail.com>
2025-11-15 12:00:00 +0000

    Shell Scripts

---
+++ string_concatenate.sh
@@ -0,0 +1,5 @@
+read -p "Enter first string: " first_string
+read -p "Enter second string: " second_string
+echo "First string is: $first_string"
+echo "Second string is: $second_string"
+echo "Concatenated string: ${first_string}${second_string}"
